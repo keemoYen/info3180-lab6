57b735a225ab10db0dc2d743b4d7b15ae944402f
Keemo <Keemoyen4@gmail.com>
2022-04-03 20:00:04 -0500

    lab6

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,14 @@
 <script>
+
+import NewsList  from '@/components/NewsList.vue'
+
 export default {
     data() {
         return {
-            message: "Hello World! Welcome to VueJS"
+            message: "My name is Keemo , And I Believe in myself"
         };
     },
+    components: { NewsList }
 }
 </script>
 
@@ -13,6 +17,9 @@ export default {
       <div class="text-center">
         <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
         <h1>{{ message }}</h1>
+        <NewsList>
+            {{components}}
+      </NewsList>
       </div>
     </div>
 </template>
